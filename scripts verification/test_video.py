a23a1c885eb9802d7a832e413a78cfa86139a486
Theo Coulongeat <theocoulongeat@gmail.com>
2026-04-22 21:42:38 +0200

    Refactor video detection script for clarity

--- scripts verification/test_video.py
+++ scripts verification/test_video.py
@@ -2,13 +2,12 @@ from ultralytics import YOLO
 import os
 
 # 1. Charge ton modèle
-model = YOLO(r"C:\Users\theoc\Desktop\Projet_SYSMER_2A\runs\detect\entrainement_sysmer2\weights\best.pt")
-#model = YOLO(r"C:\Users\theoc\Desktop\Projet_SYSMER_2A\best.pt")  
+model = YOLO(r"C:\Users\theoc\Desktop\Projet_SYSMER_2A\runs\detect\entrainement_sysmer2\weights\best.pt")  # A adapter  
 
 # 2. Chemin de ta vidéo
-video_path = r"C:\Users\theoc\Desktop\Projet_SYSMER_2A\Calibrage Direct\CAM2.avi"
+video_path = r"C:\Users\theoc\Desktop\Projet_SYSMER_2A\Calibrage Direct\CAM2.avi" # A adapter 
 
-print("🚀 Lancement de la détection...")
+print(" Lancement de la détection...")
 
 # 3. Inférence avec flux (stream)
 results = model.predict(
@@ -17,7 +16,7 @@ results = model.predict(
     save=False,   
     conf=0.5,
     stream=True, # Indispensable pour les vidéos longues
-    device=0
+    device=0   # Fait tourner l'entrainement sur le GPU
 )
 
 # 4. On DOIT parcourir le générateur pour que le travail se fasse
@@ -26,5 +25,5 @@ for r in results:
     # On récupère le dossier de sauvegarde depuis le dernier résultat traité
     path_sauvegarde = r.save_dir
 
-print(f"\n✅ Terminé !")
-print(f"👉 Ta vidéo annotée est ici : {os.path.abspath(path_sauvegarde)}")
+print(f"\n Terminé !")
+print(f"Ta vidéo annotée est ici : {os.path.abspath(path_sauvegarde)}")
